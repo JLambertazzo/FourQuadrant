b97d4a40fa708e1373b82dbe2f2da3a4ccb2e454
Julien Bertazzo Lambert <jbertazzolambert@gmail.com>
2025-03-06 20:20:32 -0500

    fix: trying to use dokploy env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
     if (mode === 'production') {
         return {
             mode: 'production',
